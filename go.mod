1f8a3586cace5e31424310f0c18983fa27fba107
Fouad <mohammad.hassan@slickblox.com>
2020-10-25 22:22:18 +0200

    deck initial commit

--- go.mod
+++ go.mod
@@ -3,27 +3,35 @@ module github.com/Praqma/helmsman
 go 1.13
 
 require (
-	cloud.google.com/go v0.38.0
+	cloud.google.com/go v0.70.0 // indirect
+	cloud.google.com/go/storage v1.12.0
 	github.com/Azure/azure-pipeline-go v0.1.9
 	github.com/Azure/azure-storage-blob-go v0.0.0-20181022225951-5152f14ace1c
 	github.com/BurntSushi/toml v0.3.1
 	github.com/apsdehal/go-logger v0.0.0-20190515211354-1abdf898e024
-	github.com/aws/aws-sdk-go v1.26.2
-	github.com/golang/groupcache v0.0.0-20191027212112-611e8accdfc9 // indirect
+	github.com/aws/aws-sdk-go v1.35.12
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/google/martian/v3 v3.1.0 // indirect
 	github.com/hashicorp/go-version v1.2.0
 	github.com/imdario/mergo v0.3.8
 	github.com/joho/godotenv v1.3.0
-	github.com/kr/pretty v0.1.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/logrusorgru/aurora v0.0.0-20191116043053-66b7ad493a23
-	github.com/pkg/errors v0.8.1 // indirect
-	go.opencensus.io v0.22.2 // indirect
-	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553
-	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6 // indirect
-	golang.org/x/sys v0.0.0-20191210023423-ac6580df4449 // indirect
-	google.golang.org/api v0.14.0 // indirect
-	google.golang.org/appengine v1.6.5 // indirect
-	google.golang.org/genproto v0.0.0-20191206224255-0243a4be9c8f // indirect
-	google.golang.org/grpc v1.25.1 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	gopkg.in/yaml.v2 v2.2.7
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/ugorji/go v1.1.13 // indirect
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+	golang.org/x/mod v0.3.1-0.20200828183125-ce943fd02449 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974
+	golang.org/x/sys v0.0.0-20201020230747-6e5568b54d1a // indirect
+	golang.org/x/tools v0.0.0-20201021203610-6652d1fb1463 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20201021134325-0d71844de594 // indirect
+	google.golang.org/grpc v1.33.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
+	gopkg.in/yaml.v2 v2.3.0
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
---
+++ internal/app/web.go
@@ -0,0 +1,107 @@
+package app
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Serve website and API
+func Serve() {
+	var s state
+
+	// delete temp files with substituted env vars when the program terminates
+	defer os.RemoveAll(tempFilesDir)
+	if !flags.noCleanup {
+		defer s.cleanup()
+	}
+
+	flags.readState(&s)
+	if len(s.GroupMap) > 0 {
+		s.TargetMap = s.getAppsInGroupsAsTargetMap()
+		if len(s.TargetMap) == 0 {
+			log.Info("No apps defined with -group flag were found, exiting...")
+			os.Exit(0)
+		}
+	}
+	if len(s.TargetMap) > 0 {
+		s.TargetApps = s.getAppsInTargetsOnly()
+		s.TargetNamespaces = s.getNamespacesInTargetsOnly()
+		if len(s.TargetApps) == 0 {
+			log.Info("No apps defined with -target flag were found, exiting...")
+			os.Exit(0)
+		}
+	}
+	settings = s.Settings
+	curContext = s.Context
+
+	// set the kubecontext to be used Or create it if it does not exist
+	log.Info("Setting up kubectl...")
+	if !setKubeContext(settings.KubeContext) {
+		if err := createContext(&s); err != nil {
+			log.Fatal(err.Error())
+		}
+	}
+
+	// add repos -- fails if they are not valid
+	log.Info("Setting up helm...")
+	if err := addHelmRepos(s.HelmRepos); err != nil && !flags.destroy {
+		log.Fatal(err.Error())
+	}
+
+	if flags.apply || flags.dryRun || flags.destroy {
+		// add/validate namespaces
+		if !flags.noNs {
+			log.Info("Setting up namespaces...")
+			if flags.nsOverride == "" {
+				addNamespaces(&s)
+			} else {
+				createNamespace(flags.nsOverride)
+				s.overrideAppsNamespace(flags.nsOverride)
+			}
+		}
+	}
+
+	if !flags.skipValidation {
+		log.Info("Validating charts...")
+		// validate charts-versions exist in defined repos
+		if err := validateReleaseCharts(&s); err != nil {
+			log.Fatal(err.Error())
+		}
+	} else {
+		log.Info("Skipping charts' validation.")
+	}
+
+	log.Info("Preparing plan...")
+	cs := buildState(&s)
+	p := cs.makePlan(&s)
+	if !flags.keepUntrackedReleases {
+		cs.cleanUntrackedReleases(&s, p)
+	}
+
+	p.sort()
+	p.print()
+	if flags.debug {
+		p.printCmds()
+	}
+	p.sendToSlack()
+
+	// if flags.apply || flags.dryRun || flags.destroy {
+	// 	p.exec()
+	// }
+	setupEndpoints(cs)
+}
+
+func setupEndpoints(cs *currentState) {
+	r := gin.Default()
+	r.GET("/releases", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusTemporaryRedirect, "/releases")
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
