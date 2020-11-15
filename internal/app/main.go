937ce86169cd5a888511711648c02b87830727d0
Fouad <mohammad.hassan@slickblox.com>
2020-11-15 23:21:10 +0200

    change version

--- internal/app/main.go
+++ internal/app/main.go
@@ -6,7 +6,7 @@ import (
 
 const (
 	helmBin            = "helm"
-	appVersion         = "v3.4.6"
+	appVersion         = "v0.1.0"
 	tempFilesDir       = ".helmsman-tmp"
 	defaultContextName = "default"
 	resourcePool       = 10
